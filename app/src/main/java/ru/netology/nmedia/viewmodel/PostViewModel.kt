f35567f35dd65cac5cd1e8d04acad94fe21ecd96
Gorbunov Nikolay <gor_bunk@mail.ru>
2022-02-02 16:44:11 +0300

    Архитектура: MVVM

---
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -0,0 +1,22 @@
+package ru.netology.nmedia.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    fun like() {
+        repository.like()
+    }
+
+    fun share() {
+        repository.share()
+    }
+
+    val data: LiveData<Post> = repository.data
+
+
+}
