660db4cc6f7f1554d3056df9f3a50c56db33b97d
Gorbunov Nikolay <gor_bunk@mail.ru>
2022-02-23 12:33:53 +0300

    Fragment done

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,4 +1,4 @@
-package ru.netology.nmedia.viewmodel
+package ru.netology.nmedia.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
@@ -6,47 +6,44 @@ import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryFileImpl
-import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
 
-val empty = Post(
-    id = 0L,
-    "",
-    "",
-    "",
-    false,
-)
+private val empty = Post()
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
-    //private val repository: PostRepository = PostRepositorySharedPrefsImpl(application)
     private val repository: PostRepository = PostRepositoryFileImpl(application)
     val data = repository.getAll()
-    fun likeById(id: Long) = repository.likeById(id)
-    fun shareById(id: Long) = repository.shareById(id)
-    fun removeById(id: Long) = repository.removeById(id)
-
-    val edited = MutableLiveData(empty)
+    private val edited = MutableLiveData(empty)
+    val currentPost = MutableLiveData(empty)
 
     fun save() {
         edited.value?.let {
             repository.save(it)
-            edited.value = empty
         }
+        currentPost.value = edited.value
+        edited.value = empty
     }
 
     fun edit(post: Post) {
         edited.value = post
     }
 
-    fun changeContent(content: String){
+
+    fun changeContent(content: String) {
         edited.value?.let {
-            if (it.content == content) {
+            val text = content.trim()
+            if (it.content == text) {
                 return
             }
-            edited.value = it.copy(content = content)
+            edited.value = it.copy(content = text)
         }
     }
 
-    fun cancelEdit() {
-        edited.value = empty
+    fun chooseCurrentPost(post: Post) {
+        currentPost.value = post
     }
+
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
+    fun removeById(id: Long) = repository.removeById(id)
+    fun findById(id: Long) = repository.findById(id)
 }
